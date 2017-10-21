0836bfaccd7832bc2bc3046f86057ff2ff9a0103
“Anrui <love_miku@icloud.com>
2017-10-20 22:10:43 -0400

    Add some example of SQL
    Add some example of SQL and successfully run

--- CS660_PA.py
+++ CS660_PA.py
@@ -4,7 +4,20 @@ from flaskext.mysql import MySQL
 app = Flask(__name__)
 mysql = MySQL()
 
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'war0623'
+app.config['MYSQL_DATABASE_DB'] = 'CS660_PA'
+app.config['MYSQL_DATABASE_HOST'] = '127.0.0.1'
+mysql.init_app(app)
 
+conn = mysql.connect()
+cursor = conn.cursor()
+query = 'select * from USER'
+cursor.execute(query)
+data = []
+for item in cursor:
+    data.append(item)
+print(data)
 
 @app.route('/')
 def index():
