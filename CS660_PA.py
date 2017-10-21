983aa14f078f55c2315c73eac9c91200e0d182a3
Ruiko <love_miku@icloud.com>
2017-10-21 00:06:21 -0400

    Add some content for login(with post problem)
    Added some content for login, but with some value post problem

--- CS660_PA.py
+++ CS660_PA.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flaskext.mysql import MySQL
 
 app = Flask(__name__)
@@ -11,19 +11,35 @@ app.config['MYSQL_DATABASE_HOST'] = '127.0.0.1'
 app.config["DEBUG"] = True
 mysql.init_app(app)
 
-conn = mysql.connect()
-cursor = conn.cursor()
-query = 'select * from USER'
-cursor.execute(query)
-data = []
-for item in cursor:
-    data.append(item)
+# conn = mysql.connect()
+# cursor = conn.cursor()
+# query = 'select * from USER'
+# cursor.execute(query)
+# data = []
+# for item in cursor:
+#     data.append(item)
 
-print(data)
+# print(data)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/register/', methods = ['POST','GET'])
+def register():
+    if request.method == 'GET':
+        print("get~")
+        return render_template('register.html')
+    if request.method == 'POST':
+        registerUserName = request.form['username']
+        registerPassword = request.form['password']
+        mysql.cursor.execute('Insert into USER (FNAME,PASSWORD) VALUES (%s,%s)',(registerUserName,registerPassword))
+        mysql.cursor.commit()
+        print("finished~")
+
+
+
+
 
 
 if __name__ == '__main__':
